933cd593efd53d6a095c874a2c0e9d2554599912
Deepika <ds7140475@gmail.com>
2024-11-22 14:15:11 +0530

    url-change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://mern-netflix-clone-ysk8.onrender.com",
       },
     },
   },
